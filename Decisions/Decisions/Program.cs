4f8a12d9b17f7c87488897bb34b0ef17d15877c4
jajton <juntillajosan@gmail.com>
2014-06-25 16:59:54 +0800

    Introducing If decision statement along with the conditional operator
    Introducing If decision statement along with the conditional operator

---
+++ Decisions/Decisions/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Please type something and press the Enter Key.");
+            //string userValue;
+            //userValue = Console.ReadLine();
+            //Console.WriteLine("You typed: " + userValue);
+            //Console.ReadLine();
+
+
+            //Console.WriteLine("Would you prefer what is behing door number 1, 2, or 3?");
+            //string userValue = Console.ReadLine();
+
+            //if (userValue == "1")
+            //{
+            //    Console.WriteLine("You won a new car!");
+            //    Console.ReadLine();
+            //}
+            //else if (userValue == "2") 
+            //{
+            //    Console.WriteLine("You won a new boat!");
+            //    Console.ReadLine();
+            //}
+            //else if (userValue == "3")
+            //{
+            //    Console.WriteLine("You won a new bike");
+            //    Console.ReadLine();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sorry, we didn't understand. You lose!");
+            //    Console.ReadLine();
+            //}
+
+            //Console.WriteLine("Would you prefer what is behing door number 1, 2, or 3?");
+            //string userValue = Console.ReadLine();
+
+            //string message = "";
+
+            //if (userValue == "1")
+            //{
+            //    message = "You won a new car!";
+            //}
+            //else if (userValue == "2")
+            //{
+            //    message = "You won a new boat!";
+            //}
+            //else if (userValue == "3")
+            //{
+            //    message = "You won a new bike!";
+            //}
+            //else
+            //{
+            //    message = "Sorry, I cant uderstand you!";
+            //}
+
+            //Console.WriteLine(message);
+            //Console.ReadLine();
+
+            Console.WriteLine("Would you prefer what is behing door number 1, 2, or 3?");
+            string userValue = Console.ReadLine();
+
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+            Console.WriteLine("You won a {0}", message);
+            Console.ReadLine();
+        }
+        
+            
+    }
+   
+}
