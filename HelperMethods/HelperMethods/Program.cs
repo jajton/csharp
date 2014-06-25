9d67dc3d424d3b967a49014bd47c41e1ef426043
jajton <juntillajosan@gmail.com>
2014-06-25 09:23:06 +0800

    Creating a helper method to break out code that may need to use in multiple places within the code. Learn about string formatting, and creat
    Creating and Calling Simple Overloaded Helper Methods

---
+++ HelperMethods/HelperMethods/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myValue1 = superSecretFormula1();
+            string myValue2 = superSecretFormula2("Sunshine");
+
+           
+
+            Console.WriteLine(myValue1);
+            Console.WriteLine(myValue2);
+       
+            Console.ReadLine();
+            Console.ReadLine();
+
+        }
+        private static string superSecretFormula1()
+        {
+            return "Hello World!";
+        }
+
+        private static string superSecretFormula2(string name)
+        {
+            return String.Format("Hi, {0}!", name);
+        }
+      
+    }
+}
