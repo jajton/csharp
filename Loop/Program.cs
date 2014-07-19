04dae2a8c1a094b9daa3092546c44852157415e9
jajton <juntillajosan@gmail.com>
2014-07-19 17:08:50 +0800

    For loop and while loop
    For loop and while loop

---
+++ Loop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int number = 5;
+
+            //for (int i = 0; i < number; i++)
+            //    Console.WriteLine(i);
+
+            //Console.ReadLine();
+
+            int number = 0;
+
+            while (number < 5)
+            {
+                Console.WriteLine(number);
+                number = number + 1;
+            }
+
+            Console.ReadLine();
+           
+        }
+    }
+}
