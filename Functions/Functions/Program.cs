b36cbc129d3680e07769d082c748f4c8f9a869dc
jajton <juntillajosan@gmail.com>
2014-07-19 17:23:59 +0800

    Learning Function Parameters
    Learning Function Parameters

---
+++ Functions/Functions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 20;
+            AddFive(ref number);
+            Console.WriteLine(number);
+            Console.ReadKey();
+        }
+
+        static void AddFive(ref int number)
+        {
+            number = number + 5;
+        }
+       
+    }
+}
